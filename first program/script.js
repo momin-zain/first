5087e1910df3249254638f4f18aeda15bedd99ca
momin-zain <arbazmomin331@gmail.com>
2025-06-18 23:28:35 +0530

    Add files via upload
    nothing

---
+++ first program/script.js
@@ -0,0 +1,14 @@
+// alert("nothing");
+function getText() {
+let temp = document.getElementById("inputId").value;
+console.log(temp);
+
+let count=0;
+for (let i of temp) {
+    if ('aeiouAEIOU'.includes(i)) {
+        count++;
+    }
+}
+
+document.getElementById("myPara").innerText="Welcome "+temp+" there are "+count+" vowels in your name.";
+}
